50f5153279d3976cc810bfa1fd36e0bc021a4fcd
Agent 84 <gitmanforhire@tutanota.com>
2021-12-07 14:14:14 +0000

    Update with my solution

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,36 @@
 fun main() {
     fun part1(input: List<String>): Int {
-        return input.size
+        var previousDepth: Int? = null
+        var currentDepth = 0
+        var increaseCount = 0
+
+        for (num in input) {
+            currentDepth = num.toInt()
+
+            if (previousDepth == null) {
+                previousDepth = currentDepth
+            }
+            else if (currentDepth > previousDepth) {
+                increaseCount++
+                previousDepth = currentDepth
+            }
+            else {
+                previousDepth = currentDepth
+            }
+        }
+
+        return increaseCount
     }
 
-    fun part2(input: List<String>): Int {
-        return input.size
-    }
+    // fun part2(input: List<String>): Int {
+    //     return input.size
+    // }
 
     // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    // val testInput = readInput("Day01_test")
+    // check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     println(part1(input))
-    println(part2(input))
+    // println(part2(input))
 }
